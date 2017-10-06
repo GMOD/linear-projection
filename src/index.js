4a1853e18c5fd467ee961e8d699b22878e729ba7
Nathan Dunn <ndunn@me.com>
2017-10-06 15:32:56 -0400

    init checkin

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+
+exports.projectValue = function (projection, input) {
+    console.log('Projecting ' + input + ' with ' + projection);
+};
+exports.unProjectValue = function (projection, input) {
+    console.log('Un projecting ' + input + ' with ' + projection);
+};
+
+exports.printMsg = function () {
+    console.log("This is a message from the demo package");
+};
